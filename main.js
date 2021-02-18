8021329f6d221a6d9d73d10bd1d6ba8690f44afe
Steven Moyano <stevenmoyano1@gmail.com>
2021-02-18 12:44:43 -0600

    completed assignment

--- main.js
+++ main.js
@@ -1,11 +1,37 @@
 /*******************
  * YOUR CODE HERE! *
  *******************/
+function yell(str) {
+  return str + "!";
+}
 
+function getFirstCharacter(str) {
+  return str[0];
+}
 
+function getLastCharacter(str) {
+  return str.charAt(str.length -1);
+}
 
+function getOneCharacter(str, num) {
+  return str.charAt(num);
+}
 
+function getTwoCharacters(str, num1, num2) {
+  return str.charAt(num1) + str.charAt(num2);
+}
 
+function makeCapitalized(str) {
+  return str.toUpperCase();
+}
+
+function yellLouder(str) {
+  return str.toUpperCase() + "!!!";
+}
+
+function getInitials(str) {
+  return str[0].toUpperCase() + "." + str.charAt(str.lastIndexOf(" ") + 1).toUpperCase() + ".";
+}
 
 /********************************************************************
  * THIS CODE IS FOR INTERNAL USE ONLY. DON'T CHANGE ANYTHING BELOW! *
